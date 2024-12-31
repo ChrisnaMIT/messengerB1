3dfa280304348754e4d27e31f4df434b2fbd6fa6
chrisna <chrisna.mitt@gmail.com>
2024-12-31 13:17:27 +0100

    faire la partie utilisateur prive

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ const pageMessagerie = document.querySelector(".pageMessagerie")
 const submitSignUP = document.querySelector(".submitSignUP")
 const submitLogin = document.querySelector(".submitLogin")
 
-async function login(username, password){
-    console.log(username, password)
+async function login(usernameLogin, passwordLogin){
+console.log(usernameLogin, passwordLogin)
 
     let paramsLogin = {
         method : "POST",
@@ -22,24 +22,50 @@ async function login(username, password){
             "Content-Type" : "application/json",
         },
         body : JSON.stringify({
-            username : username,
-            password: password,
+            username : usernameLogin.value,
+            password: passwordLogin.value,
         })
     }
 
     return await fetch('https://b1messenger.esdlyon.dev/login', paramsLogin)
         .then((response) => response.json())
         .then((json) => {
-
             return json.token
 
 
         })
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 buttonLogin.addEventListener('click', () => {
     containerSignUP.style.display = "none";
     containerLogin.style.display = "block";
+})
+
+buttonSignup.addEventListener('click', () => {
+    containerSignUP.style.display = "block";
+    containerLogin.style.display = "none";
 })
 
 submitSignUP.addEventListener('click', () => {
@@ -74,10 +100,10 @@ submitLogin.addEventListener('click', () => {
         .then((response) => {
             token = response
 
-            if (!(token===null||token===undefined)) {
+            //if (!(token===null||token===undefined)) {
                 containerLog.style.display = "none";
                 pageMessagerie.style.display = "block";
-            }
+           // }
 
 
         })
